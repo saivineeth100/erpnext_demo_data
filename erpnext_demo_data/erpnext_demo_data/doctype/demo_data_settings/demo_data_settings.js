2821136aac58a062573ebe5549796bf297fca2df
Sai Vineeth Tallapudi <64919520+saivineeth100@users.noreply.github.com>
2024-11-17 09:22:21 +0000

    added demo data settings doctype

---
+++ erpnext_demo_data/erpnext_demo_data/doctype/demo_data_settings/demo_data_settings.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2024, Sai Vineeth and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Demo Data Settings", {
+    setup: function (frm) {
+
+    },
+    refresh(frm) {
+
+    },
+});
